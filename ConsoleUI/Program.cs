aee351e0060f43cb919683ae44eab0c4791ae282
TahaKazan29 <tahakazantk@gmail.com>
2021-02-04 21:27:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,67 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Linq;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car ford = new Car
+            {
+                Id = 4,
+                BrandId = 4,
+                ColorId = 3,
+                Description = "Y",
+                DailyPrice = 2120,
+                ModelYear = "2017"
+            };
+
+            CarManager carManager = new CarManager(new EfCarDal());
+            
+            Console.WriteLine("**************** Renk Id'ye Göre Araç *****************");
+            foreach (var car in carManager.GetCarsByColorId(2))
+            {
+                Console.WriteLine("Arabanın Açıklaması: {0} Arabanın Yılı: {1} Günlük Ücret: {2}", car.Description, car.ModelYear, car.DailyPrice);
+            }
+
+            Console.WriteLine("\n**************** Marka Id'ye Göre Araç *****************");
+            foreach (var car in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine(car.Description);
+            }
+
+
+            //Console.WriteLine("\n****************Araç Eklendi***********************");
+            //carManager.Add(ford); //araç eklemek için id alanı silinmeli
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine("Arabanın Açıklaması: {0} Arabanın Yılı: {1} Günlük Ücret: {2}", car.Description, car.ModelYear, car.DailyPrice);
+            //}
+
+            //carManager.Update(ford);
+            //Console.WriteLine("\n****************Araç Güncellendi***********************");
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine("Arabanın Açıklaması: {0} Arabanın Yılı: {1} Günlük Ücret: {2}", car.Description, car.ModelYear, car.DailyPrice);
+            //}
+
+            //carManager.Delete(ford);
+            //Console.WriteLine("\n*****************Araç Silindi**********************");
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+
+
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1,BrandId = 1,ColorId = 1,DailyPrice = 300,Description="Wolksvagen Polo",ModelYear="2011"},
+                new Car{Id = 2,BrandId = 1,ColorId = 2,DailyPrice = 600,Description="Wolksvagen Golf",ModelYear="2015"},
+                new Car{Id = 3,BrandId = 2,ColorId = 3,DailyPrice = 700,Description="Audi A4",ModelYear="2014"},
+                new Car{Id = 4,BrandId = 3,ColorId = 4,DailyPrice = 900,Description="BMW 520D",ModelYear="2016"},
+                new Car{Id = 5,BrandId = 4,ColorId = 5,DailyPrice = 2300,Description="Porshe Taycan",ModelYear="2019"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.Id == carId).ToList();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
